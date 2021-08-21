1dbda6f6f8381078b4f76e7628425392195f3afe
Gloria Edem <gloedem@gmail.com>
2021-08-21 23:04:16 +0000

    modify db setup to require db.js only

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const app        = express();
 const morgan     = require('morgan');
 const cookieSession = require('cookie-session');
 
-// PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
-
+// PG database client/connection setup -- abstracted to db file!
+// const { Pool } = require('pg');
+// const dbParams = require('./lib/db.js');
+// const db = new Pool(dbParams);
+// db.connect();
+const db = require('./lib/db');
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
