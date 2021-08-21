ff4b9b8338ac01feba65b84472b9cc453985eefb
Gloria Edem <gloedem@gmail.com>
2021-08-21 21:56:52 +0000

    add /login POST functionality

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,32 @@ module.exports = (db) => {
     res.render("todo_login");
   });
   // Login Page POST Route
-
+  router.post('/login', (req, res) => {
+    const getUserByEmail = (email) => {
+      return db.query('SELECT * FROM users WHERE email = $1', [email])
+        .then((res) => {
+          console.log('db query:', res.rows[0]);
+          return res.rows[0];
+        })
+        .catch((err) => {
+          console.log('❌ query error:', err.stack);
+        });
+    };
+    const { email, password } = req.body;
+    getUserByEmail(email)
+      .then(user => {
+        //console.log('getUserByEmail form call:', user);
+        if (password === user.password) {
+          //push user object into templateVars, target those variables in index.ejs (Profile card)
+          //fire off a db query to books/movies, etc to retrieve all user lists. pass those into templateVars..
+          res.redirect('/');
+        }
+      })
+      .catch(err => {
+        console.log('error:', err.message);
+      });
+    //console.log(user);
+  });
   // Logout POST Route
 
   // Register Page GET Route
@@ -27,10 +52,10 @@ module.exports = (db) => {
   // User Page
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
-    .then(data => {
-      const users = data.rows;
-      console.log(users)
-      res.render("index");
+      .then(data => {
+        const users = data.rows;
+        console.log(users);
+        res.render("index");
       })
       .catch(err => {
         res
