e3949dfe33fedeabd92055fad63888058b72cf2b
Gloria Edem <gloedem@gmail.com>
2021-08-23 07:37:29 +0000

    modify AJAX request + API call to use similar method from books-api.js

--- public/scripts/movie-tv-api.js
+++ public/scripts/movie-tv-api.js
@@ -1,25 +1,37 @@
-$(document).ready(function () {
-  $("#movies-search").on('input', function () {
-      // target text in search box, pass that into API as query term...
-      let input = $(this).val();
-      console.log(input);
-    $.ajax({
-        url: `https://api.themoviedb.org/3/search/multi?api_key=e6b6ba40dc45cede536cb541a2b35eab&query=${input}`,
-
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            console.log("1.", data.results[0].title);
-            console.log("2.", data.results[1].title);
-            console.log("3.", data.results[2].title);
-            console.log("4.", data.results[3].title);
-            console.log("5.", data.results[4].title);
-            console.log("6.", data.results[5].title);
-            console.log("7", data.results[6].title);
-        },
-        error: function (xhr, textStatus, errorThrown) {
-            console.log('Error in Operation');
-        }
-    });
-  })
+$(document).ready(function() {
+  $("#movies-search").on('input', function() {
+    //this gets the configuration to set the base_url for poster_path...
+    // $.get(`https://api.themoviedb.org/3/configuration?api_key=e6b6ba40dc45cede536cb541a2b35eab`, function(data) {
+    //   console.log(data);
+    // })
+    // target text in search box, pass that into API as query term...
+    $('#movie-res').empty();
+    let input = $(this).val();
+    console.log(input.length);
+    //Idea: Convert AJAX req into a promise, use .then to access data to store in db...
+    if (input.length >= 4) {
+      $.get(`https://api.themoviedb.org/3/configuration?api_key=e6b6ba40dc45cede536cb541a2b35eab`, function(data) {
+        console.log(data);
+      });
+      $.get(`https://api.themoviedb.org/3/search/multi?api_key=e6b6ba40dc45cede536cb541a2b35eab&query=${input}`, function(data) {
+        console.log('API call:');
+      })
+        .then(data => {
+          const dataArr = data.results;
+          for (let i = 0; i < 2; i++) {
+            console.log(dataArr[i]);
+            //slight issue, some movies/tv shows do not have a title but have a name or original_title, how do i select which one is not undefined??? --> has to do with mediatype => Movies usually have a title, while tv shows have a name!
+            if (dataArr[i].media_type === 'movie') {
+              $('#movie-res').prepend(`<li> Title: ${dataArr[i].title}</li> <img src=https://image.tmdb.org/t/p/w154${dataArr[i].poster_path}>`);
+            } else {
+              $('#movie-res').prepend(`<li> Title: ${dataArr[i].name}</li> <img src=https://image.tmdb.org/t/p/w154${dataArr[i].poster_path}>`);
+            }
+          }
+          // store stuff in db from here? --> ON Click!
+        });
+    }
+  });
+  // Need to write query to store data in db -- CREATED fake table (books_api)
 });
+
+
