cbb8cfabc6f75a80dc22475af5e95ba89ea3b33f
cupokyle <kyleforsythcomedy@gmail.com>
2021-08-23 21:44:05 +0000

    Attempt to get restaurants api working

--- public/scripts/restaurants-api.js
+++ public/scripts/restaurants-api.js
@@ -1,48 +1,28 @@
-
 $(document).ready(function() {
-  let options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
+  const myKey = 'AIzaSyAK1_0cYdihfjuAf_yQdIzt2WY80wQsFLw';
 
-  function success(pos) {
-    let crd = pos.coords;
-
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-    const position = {"latitude": crd.latitude, "longitude": crd.longitude};
-    return position;
-  }
-
-  function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
-
-  console.log(navigator.geolocation.getCurrentPosition(success, error, options));
   $("#food-search").on('input', function() {
     // target text in search box, pass that into API as query term...
     let input = $(this).val();
-    console.log(input);
-    // console.log(geolocation)
     $.ajax({
-      url: `https://api.yelp.com/v3/autocomplete?text=${input}&latitude=${position.latitude}&longitude=${position.longitude}`,
+      url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json
+      ?input=Museum of Contemporary Art Australia
+      &inputtype=textquery
+      &fields=formatted_address,name,rating,opening_hours,geometry
+      &key=${myKey}`,
       type: 'GET',
-      dataType: 'json',
+      headers: {
+        "accept": "application/json",
+        "Access-Control-Allow-Origin":"*"
+     },
       success: function(data) {
-        console.log("1.", data);
-        console.log("2.", data);
-        console.log("3.", data);
-        console.log("4.", data);
-        console.log("5.", data);
-        console.log("6.", data);
-        console.log("7", data);
+        console.log("success", data);
       },
       error: function(xhr, textStatus, errorThrown) {
         console.log('Error in Operation');
       }
-    });
+    }).then(res => {
+      console.log('Result:', res);
+    })
   });
 });
