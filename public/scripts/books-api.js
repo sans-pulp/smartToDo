c6a6fad6643e7ba7f8a43b9a2c1c8a459a5999dd
Gloria Edem <gloedem@gmail.com>
2021-08-24 18:25:25 +0000

    wrap logic in event listener for static #search element since everything is dynamically generated

--- public/scripts/books-api.js
+++ public/scripts/books-api.js
@@ -1,49 +1,53 @@
 $(() => {
-  let $bookList = [];
-  $("#book-search").on('input', function() {
-    // target text in search box, pass that into API as query term...
-    $('#book-res').empty();
-    let input = $(this).val();
-    console.log(input.length);
-    //Idea: Convert AJAX req into a promise, use .then to access data to store in db...
-    if (input.length >= 4) {
-      $.get(`https://www.googleapis.com/books/v1/volumes?q=${input}`, function(data) {
-        console.log('API call:');
-      })
-        .then(data => {
-          const dataArr = data.items;
-          //console.log('dataArr', dataArr);
-          $bookList = data.items;
-          for (let i = 0; i < 5; i++) {
+  $('#search').on('click', '.item-input', function() {
 
-            $('#book-res').append(`<li data-id="${i}" id="book${i + 1}"><p class="title">Title: ${dataArr[i].volumeInfo.title}</p><p class="author">Author(s): ${dataArr[i].volumeInfo.authors}</p><img class="thumbnail" src=${dataArr[i].volumeInfo.imageLinks.thumbnail} /><button>Select this</button></li> `);
-          }
+    let $bookList = [];
+    $("#book-search").on('input', function() {
+      // target text in search box, pass that into API as query term...
+      $('#book-res').empty();
+      let input = $(this).val();
+      console.log(input.length);
+      //Idea: Convert AJAX req into a promise, use .then to access data to store in db...
+      if (input.length >= 4) {
+        $.get(`https://www.googleapis.com/books/v1/volumes?q=${input}`, function(data) {
+          console.log('API call:');
+        })
+          .then(data => {
+            const dataArr = data.items;
+            //console.log('dataArr', dataArr);
+            $bookList = data.items;
+            for (let i = 0; i < 5; i++) {
 
-        });
+              $('#book-res').append(`<li data-id="${i}" id="book${i + 1}"><p class="title">Title: ${dataArr[i].volumeInfo.title}</p><p class="author">Author(s): ${dataArr[i].volumeInfo.authors}</p><img class="thumbnail" src=${dataArr[i].volumeInfo.imageLinks.thumbnail} /><button>Select this</button></li> `);
+            }
 
-    }
-  });
+          });
 
-  $('#book-res').on('click', 'li', function() {
-    //how would I access dataArr[i] when i click the Select this button?
-    // onclick i want to do selectBook(dataArr[i])
-    // Do something on an existent or future .dynamicElement
-
-    let dataSelected = $(this).attr("data-id");
-    console.log('______', dataSelected);
-    const bookInfo = $bookList[dataSelected];
-    console.log('++++', bookInfo);
-    const userId = $("#userid").val();
-    console.log('userId', userId);
-    const bookObj = { user: userId, title: bookInfo.volumeInfo.title, author: bookInfo.volumeInfo.authors[0], publisher: bookInfo.volumeInfo.publisher, image: bookInfo.volumeInfo.imageLinks.thumbnail };
-    console.log(bookObj);
-    $.post("/api/books/new", bookObj)
-      .done(function(data) {
-        alert("Book added" + data);
-        window.location = "/";
+      }
     });
 
-  });
+    $('#book-res').on('click', 'li', function() {
+      //how would I access dataArr[i] when i click the Select this button?
+      // onclick i want to do selectBook(dataArr[i])
+      // Do something on an existent or future .dynamicElement
+
+      let dataSelected = $(this).attr("data-id");
+      console.log('______', dataSelected);
+      const bookInfo = $bookList[dataSelected];
+      console.log('++++', bookInfo);
+      const userId = $("#userid").val();
+      console.log('userId', userId);
+      const bookObj = { user: userId, title: bookInfo.volumeInfo.title, author: bookInfo.volumeInfo.authors[0], publisher: bookInfo.volumeInfo.publisher, image: bookInfo.volumeInfo.imageLinks.thumbnail };
+      console.log(bookObj);
+      $.post("/api/books/new", bookObj)
+        .done(function(data) {
+          alert("Book added" + data);
+          window.location = "/";
+      });
+
+    });
+
+  })
 
 });
 
