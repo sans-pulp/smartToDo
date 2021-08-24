7cb3eba1549c98e71cbc588d48abc66bf6b6d61b
Gloria Edem <gloedem@gmail.com>
2021-08-24 12:08:14 +0000

    add queries to get or add books to db table

---
+++ db/queries/book-queries.js
@@ -0,0 +1,29 @@
+const db = require('../../lib/db');
+
+const getAllBooks = () => {
+  return db.query('SELECT * FROM books_api')
+    .then((res) => {
+      console.log('getAllBooks query:', res.rows);
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log('❌ getAllBooks query error:', err.stack);
+    });
+};
+
+const addBook = (bookObj, user_id) => {
+  const query = `INSERT INTO books_api (user_id, title, author, publisher, image_thumbnail) VALUES ($1, $2, $3, $4, $5) RETURNING * `;
+  const values = [user_id, bookObj.title, bookObj.author, bookObj.publisher, bookObj.image_thumbnail];
+  return db.query(query, values)
+    .then((res) => {
+      console.log('addBook query', res.rows);
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log('❌ addBook query error:', err.stack);
+    });
+};
+
+module.exports = {
+  getAllBooks, addBook
+};
