6a98b32ad110bb5a17d9dc0239e4439483774e1d
Gloria Edem <gloedem@gmail.com>
2021-08-24 14:57:55 +0000

    add getBooksById query

--- db/queries/book-queries.js
+++ db/queries/book-queries.js
@@ -1,7 +1,7 @@
 const db = require('../../lib/db');
 
 const getAllBooks = () => {
-  return db.query('SELECT * FROM books_api')
+  return db.query('SELECT * FROM books_api;')
     .then((res) => {
       console.log('getAllBooks query:', res.rows);
       return res.rows;
@@ -11,8 +11,20 @@ const getAllBooks = () => {
     });
 };
 
+const getBooksById = (id) => {
+  const query = 'SELECT * FROM books_api WHERE id = $1;';
+  return db.query(query, [id])
+    .then((res) => {
+      console.log('getAllBooks query:', res.rows);
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log('❌ getBooksById query error:', err.stack);
+    });
+};
+
 const addBook = (bookObj, user_id) => {
-  const query = `INSERT INTO books_api (user_id, title, author, publisher, image_thumbnail) VALUES ($1, $2, $3, $4, $5) RETURNING * `;
+  const query = `INSERT INTO books_api (user_id, title, author, publisher, image_thumbnail) VALUES ($1, $2, $3, $4, $5) RETURNING *; `;
   const values = [user_id, bookObj.title, bookObj.author, bookObj.publisher, bookObj.image_thumbnail];
   return db.query(query, values)
     .then((res) => {
@@ -25,5 +37,5 @@ const addBook = (bookObj, user_id) => {
 };
 
 module.exports = {
-  getAllBooks, addBook
+  getAllBooks, getBooksById, addBook
 };
