158ea8437f1aa277814756fb2d93a1c36ffb6fe7
Gloria Edem <gloedem@gmail.com>
2021-08-24 11:34:58 +0000

    attempt to add onClick fnc to button to select book for POST req - this did not work as the function kept returning as undefined

--- public/scripts/books-api.js
+++ public/scripts/books-api.js
@@ -1,6 +1,12 @@
-$(document).ready(function() {
+$(() => {
   $("#book-search").on('input', function() {
     // target text in search box, pass that into API as query term...
+    let bookchoice;
+    //create fnc to pass in obj and assigns the obj to the var to store the selected book
+    const selectBook = obj => {
+      bookchoice = obj;
+      // return bookchoice;
+    };
     $('#book-res').empty();
     let input = $(this).val();
     console.log(input.length);
@@ -11,17 +17,38 @@ $(document).ready(function() {
       })
         .then(data => {
           const dataArr = data.items;
-          for (let i = 0; i < 2; i++) {
-            //console.log(dataArr[i].volumeInfo.title, dataArr[i].volumeInfo.authors);
-            $('#book-res').prepend(`<li> Title: ${dataArr[i].volumeInfo.title}, Author: ${dataArr[i].volumeInfo.authors}</li> <img src=${dataArr[i].volumeInfo.imageLinks.thumbnail}>`);
+          const clickyFnc = (i, obj) => {
+            selectBook(obj[i]);
           }
-          const books = { "one": dataArr[0].volumeInfo, "two": dataArr[1].volumeInfo}
-          return books;
-          // store stuff in db from here? --> ON Click!
+          for (let i = 0; i < 2; i++) {
+            console.log('volInfo', dataArr[i].volumeInfo);
+            $('#book-res').append(`<li id="book${i + 1}"><button onclick="clickyFnc(i, dataArr)()" > Title: ${dataArr[i].volumeInfo.title}, Author: ${dataArr[i].volumeInfo.authors} <img src=${dataArr[i].volumeInfo.imageLinks.thumbnail}> </button> </li> `);
+          }
+          return bookchoice;
+
+          
+          // create form with checkbox options
+          //options are search results
+          //once form is populated by for loop, query form to access values checked
+          //extract values (array), map them to values in dataArray to get corresponding values (based on index)
+          // send form data to db...
+
         })
-        .then(book_data =>{
-          console.log('api data', book_data);
-          //const books = { "first": {}, "second": {} }
+        .then((bookchoice) => {
+          console.log('api data', bookchoice);
+          //create var to store selected book
+
+
+          //fnc to post to server
+          const createBook = bookObj => {
+            const query = `INSERT INTO books_api (title, author, publisher, image_thumbnail) VALUES ($1, $2, $3, $4) RETURNING *`;
+            const values = [bookObj.title, bookObj.authors[0], bookObj.publisher, bookObj.imageLinks.thumbnail];
+            return db.query(query, values);
+          };
+
+          //$('#book')
+
+          // if i want the user to be able to select/click one of the li's to send the data into the db, how would I go about it?
         //how would I get this data sent into the db? is it a post req?
         });
     }
@@ -34,7 +61,7 @@ $(document).ready(function() {
 - title
 - authors[0] - first author
 - publisher
-- categories[0] -- first category
+// - categories[0] -- first category
 - imageLinks.thumbnail
   - get default image in case imageLinks.thumbnail does not exist
 */
