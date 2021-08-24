a66f9bfafe041107fc20a709e792ed2888104057
cupokyle <kyleforsythcomedy@gmail.com>
2021-08-24 22:49:47 +0000

    Get rakuten api working. Still not ideal. Try Resy for restaurants. Not working yet

---
+++ public/scripts/products-api.js
@@ -0,0 +1,59 @@
+const { rakutenKey } = require('keys/rakuten-key.js')
+$(() => {
+  $('#search').on('click', '.item-input', function() {
+    function isEnglish(str) {
+      return str[0].match(/[a-z]/i);
+    }
+    function onlyEnglish(str){
+      let solution = "";
+      for (let i = 0; i < str.length; i++) {
+        if(str[i] === " " || isEnglish(str[i])){
+          solution += str[i]
+        }
+      }
+      return solution;
+    }
+        let $productList = [];
+        $("#product-search").on('input', function() {
+          // target text in search box, pass that into API as query term...
+          $('#product-res').empty();
+          setTimeout(() => {
+            while ($('#product-res').length > 2){
+              $('#product-res').length = 2;
+            }
+          }, 1000)
+          let input = $(this).val();
+          console.log(input.length);
+          if (input.length > 3) {
+          //Idea: Convert AJAX req into a promise, use .then to access data to store in db...
+          const settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": `https://rakuten_webservice-rakuten-marketplace-product-search-v1.p.rapidapi.com/services/api/Product/Search/20170426?keyword=${input}`,
+            "method": "GET",
+            "headers": {
+              "x-rapidapi-host": "rakuten_webservice-rakuten-marketplace-product-search-v1.p.rapidapi.com",
+              "x-rapidapi-key": rapidKey()
+            }
+          };
+
+          $.ajax(settings).done(function (response) {
+            console.log(response);
+          })
+          .then(data => {
+            const dataArr = data.Products;
+            $productList = data.items;
+            for (let i = 0; i < 2; i++) {
+              if(isEnglish(dataArr[i].Product.productName)){
+
+                //dynamically show search results on page
+                $('#product-res').append(`<li data-id="${i}" id="product${i + 1}"><p class="title"> Title: ${onlyEnglish(dataArr[i].Product.productName)}<img class="thumbnail" src=${dataArr[i].Product.smallImageUrl} /><p> Price: $${dataArr[i].Product.averagePrice / 100}</p><button>Select this</button></li> `);
+
+              }
+            }
+
+          });
+        };
+          });
+});
+});
--- public/scripts/restaurants-api.js
+++ public/scripts/restaurants-api.js
@@ -1,45 +1,24 @@
-$(document).ready(function() {
-  const myKey = 'INSERT KEY';
+const { rapidKey } = require('keys/rapid-key.js')
 
-  function initMap() {
-    const sydney = new google.maps.LatLng(-33.867, 151.195);
-    infowindow = new google.maps.InfoWindow();
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: sydney,
-      zoom: 15,
-    });
-    const request = {
-      query: "Museum of Contemporary Art Australia",
-      fields: ["name", "geometry"],
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-        for (let i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-        }
-        map.setCenter(results[0].geometry.location);
-      }
-    });
-  }
+$(() => {
+  $('#search').on('click', '.item-input', function() {
+        let $restaurantList = [];
 
   $("#food-search").on('input', function() {
-    // target text in search box, pass that into API as query term...
-    // let input = $(this).val();
-    // $.ajax({
-    //   url: `https://api.yelp.com/v3/businesses/WavvLdfdP6g8aZTtbBQHTw`,
-    //   type: 'GET',
-    //   'Access-Control-Allow-Origin': 'http://localhost:8080/',
-    //     'Access-Control-Allow-Methods':'GET', 'Authorization': 'Bearer GixEmjn2yx9r_eSrRV1uGJ7nC-Vzq835hv8hldXFmxG5bg2s0kHWghgrFT-MVDn30lQWkxvYgskYF58yBCZyQFxMC7ZT6wIInYAhJbdH1eTz9Aj33mc9kjZeLVUkYXYx',
-    //   success: function(data) {
-    //     console.log("success", data);
-    //   },
-    //   error: function(xhr, textStatus, errorThrown) {
-    //     console.log('Error in Operation');
-    //   }
-    // }).then(res => {
-    //   console.log('Result:', res);
-    // })
-    initMap();
+    const settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://resy.p.rapidapi.com/4/find?lat=37.788719679657554&long=-122.40057774847898&day=2021-02-14&party_size=2&offset=0",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "resy.p.rapidapi.com",
+        "x-rapidapi-key": rapidKey()
+      }
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
   })
+})
 });
