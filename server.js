5e47456ab4ccdc952b477b5ba00353f7cc43aaab
Gloria Edem <gloedem@gmail.com>
2021-08-24 12:10:21 +0000

    mount /books route from router file

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ app.use(cookieSession({
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-
+const booksRoutes = require("./routes/books-router")
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/", usersRoutes(db));
-
+app.use("/books", booksRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
 
