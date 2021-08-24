3382c5aa309c881ee1287f5cffdb4270c6c0afb2
Gloria Edem <gloedem@gmail.com>
2021-08-24 15:33:57 +0000

    add GET /books/:id route

--- routes/books-router.js
+++ routes/books-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllBooks, addBook } = require('../db/queries/book-queries');
+const { getAllBooks, getBooksByUserId ,addBook } = require('../db/queries/book-queries');
 
 module.exports = (db) => {
   // GET /books
@@ -11,5 +11,14 @@ module.exports = (db) => {
       });
 
   });
+  //how would I get books by the userID and show on index?? I have the query, can i use req.session?
+  router.get("/:id", (req, res) => {
+    const userId = req.session.user_id;
+    getBooksByUserId(userId)
+      .then(book => {
+        res.json(book);
+      });
+  });
+
   return router;
 };
