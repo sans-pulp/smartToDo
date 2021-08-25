d21d9ebd18f4fd9a8a9341869841baab44862528
Gloria Edem <gloedem@gmail.com>
2021-08-25 01:17:03 +0000

    scrapped YelpAPI - moving on to GooglePlaces API to avoid CORS issue

--- public/scripts/restaurants-api.js
+++ public/scripts/restaurants-api.js
@@ -1,28 +1,12 @@
 $(document).ready(function() {
-  const myKey = 'INSERT API KEY';
+  
+  $('#search').on('click', '.item-input', function() {
 
-  $("#food-search").on('input', function() {
-    // target text in search box, pass that into API as query term...
-    let input = $(this).val();
-    $.ajax({
-      url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json
-      ?input=Museum of Contemporary Art Australia
-      &inputtype=textquery
-      &fields=formatted_address,name,rating,opening_hours,geometry
-      &key=${myKey}`,
-      type: 'GET',
-      headers: {
-        "accept": "application/json",
-        "Access-Control-Allow-Origin":"*"
-     },
-      success: function(data) {
-        console.log("success", data);
-      },
-      error: function(xhr, textStatus, errorThrown) {
-        console.log('Error in Operation');
-      }
-    }).then(res => {
-      console.log('Result:', res);
-    })
+    $("#food-search").on('input', function() {
+      // target text in search box, pass that into API as query term...
+      let input = $(this).val();
+
+    });
   });
 });
+
