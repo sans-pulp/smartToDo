9e7269c3a7f1f89ad433d4955ab62a0e95b99294
Gloria Edem <gloedem@gmail.com>
2021-08-25 12:30:24 +0000

    implement delete functionality for book items

--- public/scripts/delete-items.js
+++ public/scripts/delete-items.js
@@ -2,15 +2,15 @@ $(() => {
   //delete tv show
   $('#todo-list').on('click', '#del-btn-tv' , function() {
     const id = $(this).find('.itemId').val();
-    console.log($(this));
-    console.log(id);
+    //console.log($(this));
+    //console.log(id);
     // const id = $('#todo-list').find("#itemId").val();
     $.ajax({
       url: `/api/movies/${id}`,
       type: 'DELETE',
       success: function(res) {
         alert('TV Show Deleted!');
-        window.location = "/"
+        window.location = "/";
       }
     });
   });
@@ -18,17 +18,33 @@ $(() => {
   //delete movie
   $('#todo-list').on('click', '#del-btn-movie' , function() {
     const id = $(this).find('.itemId').val();
-    console.log($(this));
-    console.log(id);
+    //console.log($(this));
+    //console.log(id);
     // const id = $('#todo-list').find("#itemId").val();
     $.ajax({
       url: `/api/movies/${id}`,
       type: 'DELETE',
       success: function(res) {
         alert('Movie Deleted!');
-        window.location = "/"
+        window.location = "/";
       }
     });
   });
 
+
+  //delete book
+  $('#todo-list').on('click', '#del-btn-book' , function() {
+    const id = $(this).find('.itemId').val();
+    //console.log($(this));
+    //console.log(id);
+    // const id = $('#todo-list').find("#itemId").val();
+    $.ajax({
+      url: `/api/books/${id}`,
+      type: 'DELETE',
+      success: function(res) {
+        alert('Book Deleted!');
+        window.location = "/";
+      }
+    });
+  });
 });
