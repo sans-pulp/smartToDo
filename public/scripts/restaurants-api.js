52ed1c634c22ee3c0777f9407f0fb7cf1454bfce
cupokyle <kyleforsythcomedy@gmail.com>
2021-08-25 19:00:17 +0000

    clear out restaurants file as nothing has worked so far

--- public/scripts/restaurants-api.js
+++ public/scripts/restaurants-api.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $('#search').on('click', '.item-input', function() {
 
     $("#food-search").on('input', function() {
+      console.log("KKSSKDSKDN")
       // target text in search box, pass that into API as query term...
       let input = $(this).val();
 
